092418ad40b073b9f9c779b7d26fff73387aaf8b
Krupal <krupal.patel.23@gmail.com>
2024-03-08 21:30:39 -0500

    added logger

---
+++ project_1/logger.py
@@ -0,0 +1,26 @@
+import logging
+import logging.config
+from pathlib import Path
+
+import yaml
+
+
+class Logger:
+    def get_logger(self) -> logging.Logger:
+        log_config_path: str = "logging.yaml"
+
+        logfile = "logs/project.log"
+
+        if Path(log_config_path).exists():
+            with Path.open(Path(log_config_path)) as f:
+                config_ = yaml.safe_load(f.read())
+            config_["handlers"]["info_file_handler"]["filename"] = logfile
+            logging.config.dictConfig(config_)
+        else:
+            print("Unable to load the config using file. Using basic config")
+            logging.basicConfig(level=logging.INFO)
+
+        return logging.getLogger()
+
+
+log: logging.Logger = Logger().get_logger()
